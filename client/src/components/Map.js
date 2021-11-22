f4289f8e413cb467aa52ed069c4ef3d4631a38d1
dillerwin <dillerwin@gmail.com>
2021-11-21 23:40:05 -0500

    pin onClick working, heading for styling now

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -7,15 +7,6 @@ function Map(props) {
     return null;
   }
 
-  // function MarkerClick({ center, zoom }) {
-  //   const map = useMapEvent("click", (event) => {
-  //     map.setView(center, zoom);
-  //     console.log(`clicked`);
-  //     props.setdiner(event.target.id);
-  //   });
-  //   return null;
-  // }
-
   function handleClick(event) {
     console.log(`clicked`);
     event.preventDefault();
@@ -38,31 +29,93 @@ function Map(props) {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
       {/* markers for diners */}
-      <Marker id="als" position={[44.467162, -73.1765308]}>
+      <Marker
+        id="als"
+        position={[44.467162, -73.1765308]}
+        eventHandlers={{
+          click: (event) => {
+            props.setdiner(`als`);
+          },
+        }}
+      >
         <Popup id="als">Al's French Fries</Popup>
       </Marker>
-      <Marker id="athens" position={[44.5036718, -73.184801]}>
+      <Marker
+        id="athens"
+        position={[44.5036718, -73.184801]}
+        eventHandlers={{
+          click: (event) => {
+            props.setdiner(`athens`);
+          },
+        }}
+      >
         <Popup>Athen's Diner</Popup>
       </Marker>
-      <Marker id="common" position={[44.1488783, -72.6584726]}>
+      <Marker
+        id="common"
+        position={[44.1488783, -72.6584726]}
+        eventHandlers={{
+          click: (event) => {
+            props.setdiner(`common`);
+          },
+        }}
+      >
         <Popup>The Common Cafe</Popup>
       </Marker>
-      <Marker id="creek-house" position={[43.8096639, -72.652306]}>
+      <Marker
+        id="creek-house"
+        position={[43.8096639, -72.652306]}
+        eventHandlers={{
+          click: (event) => {
+            props.setdiner(`creek-house`);
+          },
+        }}
+      >
         <Popup>Creek House Diner</Popup>
       </Marker>
-      <Marker id="four-aces" position={[43.6501609, -72.3145]}>
+      <Marker
+        id="four-aces"
+        position={[43.6501609, -72.3145]}
+        eventHandlers={{
+          click: (event) => {
+            props.setdiner(`four-aces`);
+          },
+        }}
+      >
         <Popup>Four Ace's Diner</Popup>
       </Marker>
-      <Marker id="henrys" position={[44.4780127, -73.215699]}>
+      <Marker
+        id="henrys"
+        position={[44.4780127, -73.215699]}
+        eventHandlers={{
+          click: (event) => {
+            props.setdiner(`henrys`);
+          },
+        }}
+      >
         <Popup>Henry's Diner</Popup>
       </Marker>
-      <Marker id="red-arrow" position={[42.9935086, -71.4636055]}>
+      <Marker
+        id="red-arrow"
+        position={[42.9935086, -71.4636055]}
+        eventHandlers={{
+          click: (event) => {
+            props.setdiner(`red-arrow`);
+          },
+        }}
+      >
         <Popup>Red Arrow Diner</Popup>
       </Marker>
-      <Marker id="village" position={[44.5041242, -72.3696713]}>
-        <Popup id="village" onClick={handleClick}>
-          Village Restaurant
-        </Popup>
+      <Marker
+        id="village"
+        position={[44.5041242, -72.3696713]}
+        eventHandlers={{
+          click: (event) => {
+            props.setdiner(`village`);
+          },
+        }}
+      >
+        <Popup>Village Restaurant</Popup>
       </Marker>
     </MapContainer>
   );
